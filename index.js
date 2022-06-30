6cd544362f22788d26e91b1c30842b3f43e78b18
Owura Asare <oka217@nyu.edu>
2022-06-30 13:33:00 -0400

    body parser 2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
 const { Client } = require('pg');
